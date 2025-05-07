7d450b4aadb13df5a3ff488ca7b0a8f098cc139b
Alan Sung <ovis35@users.noreply.github.com>
2025-05-07 15:41:15 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/taipei-strong-web/', // 請確認 'taipei-strong-web' 是您正確的儲存庫名稱
+
   server: {
     host: "::",
     port: 8080,
